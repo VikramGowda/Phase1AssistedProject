18e29d031cdd231aaae65e337057d5100b22a643
VikramGowda <90854599+VikramGowda@users.noreply.github.com>
2022-01-22 14:49:02 +0530

    Add files via upload

--- main.java
+++ main.java
@@ -1,25 +1,20 @@
-package keywords;
+package queue;
 
+import java.util.*;
 
-class MyException extends Exception 
-{ 
-    public MyException(String s) 
-    { 
-        super(s); 
-    } 
-} 
-public class main 
-{ 
-    public static void main(String args[]) 
-    { 
-        try
-        { 
-            throw new MyException("temp"); 
-        } 
-        catch (MyException ex) 
-        { 
-            System.out.println("Caught"); 
-            System.out.println(ex.getMessage()); 
-        } 
-    } 
+public class main {
+public static void main(String[] args) 
+{
+        		Queue<String> locationsQueue = new LinkedList<>();
+                locationsQueue.add("India");
+        		locationsQueue.add("Karnataka");
+        		locationsQueue.add("TamilNadu");
+        		locationsQueue.add("Kerala");
+        		locationsQueue.add("AndraPradesh");
+System.out.println("Queue is : " + locationsQueue);
+        		System.out.println("Head of Queue : " + locationsQueue.peek());
+        		locationsQueue.remove();
+        		System.out.println("After removing Head of Queue : " + locationsQueue);
+        		System.out.println("Size of Queue : " + locationsQueue.size());
+    	}
 }
